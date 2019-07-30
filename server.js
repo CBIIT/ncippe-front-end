81eb229bae382385be34f6d39818382d4eac5704
alifr <frank.ali@nih.gov>
2019-07-29 23:06:37 -0400

    adding singular to reduce json-server single array object to just an object

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const router = jsonServer.router('./src/data/mockData.json');
 const middlewares = jsonServer.defaults({
   static: __dirname + '/build/'
 });
+const singularMiddleware = require('./server_singular')
+
 const port = process.env.PORT || 5000;
 
 server.use(middlewares)
+server.use(singularMiddleware)
 server.use('/api', router)
 
 server.listen(port,(req, res) => console.log( `server listening on port: ${port}`));
