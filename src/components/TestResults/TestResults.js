dd6f9b8a134b37ba3cc6909b5be3e18f25fb6c49
alifr <frank.ali@nih.gov>
2019-08-28 14:49:45 -0400

    remove mixed permission level views

--- src/components/TestResults/TestResults.js
+++ src/components/TestResults/TestResults.js
@@ -8,12 +8,6 @@ import TestResultsItem from './TestResultsItem'
 import { formatPhoneNumber } from '../../utils/utils'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'block'
-  },
-  grid: {
-    marginBottom: theme.spacing(3)
-  },
   header: {
     margin: theme.spacing(3, 0)
   },
@@ -22,27 +16,15 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const TestResults = (props) => {
+const TestResults = () => {
   const classes = useStyles()
   const [loginContext, dispatch] = useContext(LoginContext)
   const [reports, setReports] = useState(false)
-  const [user, setUser] = useState(false)
 
   useEffect(() => {
-    //fetch mock users list
-    const {userName, token, env, reports, userType, userGUID} = loginContext
-    // const userID = props.location.state? props.location.state.userName : userName
-    if(userType === 'PPE_PARTICIPANT') {
-      if(reports && reports.length > 0) {
-        setReports(reports)
-      }
-    } else {
-      const patientID = props.userName || userName
-      const patientGUID = loginContext.patients.find(patient => patient.userName === props.userName).userGUID
-      api[env].fetchPatientTestResults({userGUID: patientGUID, userName: patientID, token}).then(resp => {
-        setReports(resp.reports)
-        setUser(resp)
-      })
+    const {reports} = loginContext
+    if(reports && reports.length > 0) {
+      setReports(reports)
     }
   }, [])
 
@@ -51,20 +33,11 @@ const TestResults = (props) => {
       <Typography variant="h2" className={classes.header}>
         Biomarker Tests
       </Typography>
-      {user && (
-        <>
-          <Typography variant="h6">{user.firstName} {user.lastName}</Typography>
-          <Typography component="p"><a href={`mailto:${user.email}`}>{user.email}</a></Typography>
-          <Typography component="p">{formatPhoneNumber(user.phoneNumber)}</Typography>
-        </>
-      )}
       {reports ? (
         <>
-          {!user && (
-            <Typography gutterBottom>The NCI lab generates a biomarker report after we collect your biosample. Your reports are available below. Your provider has access to your reports as well.</Typography>
-          )}
+          <Typography gutterBottom>The NCI lab generates a biomarker report after we collect your biosample. Your reports are available below. Your provider has access to your reports as well.</Typography>
           <Divider className={classes.divider} />
-          <Grid container className={classes.controlsGrid} spacing={3} alignItems="center">
+          <Grid container className={classes.controlsGrid} spacing={3} alignItems="stretch">
             {reports && reports.map((report,i) => <TestResultsItem key={i} report={report} />)}
           </Grid>
         </>
