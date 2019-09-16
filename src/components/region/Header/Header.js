8a926f2e84094cf2e56978b81bfc0edea63ca2d0
alifr <frank.ali@nih.gov>
2019-09-16 10:34:43 -0400

    update header and footer for mobile

--- src/components/region/Header/Header.js
+++ src/components/region/Header/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
+  Box,
   Button,
   Container,
   Link,
@@ -12,23 +13,33 @@ import LoginButton from '../../login/SharedLogin/LoginButton'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginBottom: theme.spacing(3)
+    margin: theme.spacing(2,0)
   },
   appToolbarContainer: {
     display: 'flex',
     alignItems: 'center',
+    padding: 0
   },
   toolbarLogo: {
     flexGrow: 1,
     maxHeight: 50,
     margin: 0,
-    marginTop: theme.spacing(3),
+    
 
     '& img': {
       height: 50,
       width: 'auto',
       maxWidth: '100%',
     },
+  },
+  headerLink: {
+    color: theme.palette.grey[800],
+    marginRight: theme.spacing(1),
+    
+    '&:hover': {
+      color: theme.palette.grey[900],
+      fontWeight: '600'
+    }
   },
   mockUsers: {
     marginRight: theme.spacing(1)
@@ -37,20 +48,38 @@ const useStyles = makeStyles(theme => ({
 
 const Header = () => {
   const classes = useStyles()
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 800)
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setIsMobile(window.innerWidth < 800)
+    })
+  },[isMobile])
   return (
-    <Typography className={classes.root} component="header">
-      <Container className={classes.appToolbarContainer}>
-        <figure className={classes.toolbarLogo}>
-          <Link component={RouterLink} to='/'>
-            <img src={`/${process.env.PUBLIC_URL}assets/images/nci-ppe-logo.svg`} alt='NCI PPE logo' title='NCI Patient and Provider Engagement Portal' />
+    <Container>
+      <Typography className={classes.root} component="header">
+        <Box className={classes.appToolbarContainer}>
+          <figure className={classes.toolbarLogo}>
+            <Link component={RouterLink} to='/'>
+              <img src={`/${process.env.PUBLIC_URL}assets/images/nci-ppe-logo.svg`} alt='NCI PPE logo' title='NCI Patient and Provider Engagement Portal' />
+            </Link>
+          </figure>
+          {!isMobile && 
+          <nav className={classes.publicNavDesktop}>
+            <Link component={RouterLink} to='/expect' className={classes.headerLink}>What to expect</Link>
+          </nav>
+          }
+          <Link component={RouterLink} to='/dashboard/mock-users' className={classes.mockUsers}>
+            <Button variant="outlined" color="primary">Mock User</Button>
           </Link>
-        </figure>
-        <Link className={classes.mockUsers} component={RouterLink} to='/dashboard/mock-users'>
-          <Button variant="outlined" color="primary">Mock User</Button>
-        </Link>
-        <LoginButton />
-      </Container>
-    </Typography>
+          <LoginButton />
+        </Box>
+        {isMobile && <nav className={classes.publicNavMobile}>
+          <Link component={RouterLink} to='/expect' className={classes.headerLink}>What to expect</Link>
+        </nav>
+        }
+      </Typography>
+    </Container>
   )
 }
 
