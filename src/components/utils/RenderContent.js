11624a81c2343227305717ee36546ecf9ec9ceb9
alifr <frank.ali@nih.gov>
2020-01-28 13:38:10 -0500

    check for children before accessing props

--- src/components/utils/RenderContent.js
+++ src/components/utils/RenderContent.js
@@ -3,10 +3,10 @@ import ReactMarkdown from 'react-markdown/with-html'
 
 const root = props => {
   // console.log(props)
-  if(props.children[0].props.children) {
+  if(props.children[0] && props.children[0].props.children) {
     return <Fragment>{props.children[0].props.children}</Fragment>
   }
-  if(props.children[0].props.element) {
+  if(props.children[0] && props.children[0].props.element) {
     return <Fragment>{props.children[0].props.element}</Fragment>
   }
   if(props.children) {
