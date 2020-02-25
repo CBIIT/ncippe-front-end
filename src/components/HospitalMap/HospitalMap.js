30ffc750bc19cee50357d79b38a6846bb59526c9
alifr <frank.ali@nih.gov>
2020-02-25 16:15:12 -0500

    Big update for hospital map interaction

--- src/components/HospitalMap/HospitalMap.js
+++ src/components/HospitalMap/HospitalMap.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Grid, Link, Paper, Typography } from '@material-ui/core'
+import { Box, Divider, Grid, Link, Paper, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { useScript } from '../../components/utils/useScript'
 import { useTranslation } from 'react-i18next'
@@ -22,23 +22,31 @@ const useStyles = makeStyles( theme => ({
   locations: {
     height: 480,
     overflow: 'auto',
-
-    '& > a': {
-      display: 'block',
-      margin: theme.spacing(0,3),
-      padding: theme.spacing(3,0),
-      borderBottom: `1px solid #ccc`,
+  },
+  location: {
+    display: 'block',
+    margin: theme.spacing(1),
+    padding: theme.spacing(1,0),
+    lineHeight: 'normal',
+    textDecoration: 'none',
+    color: theme.palette.text.primary,
+    cursor: 'pointer',
+    '& > div': {
+      borderRadius: theme.spacing(1),
+      padding: theme.spacing(2),
+    },
+    '& p': {
       lineHeight: 'normal',
+    },
+    '&:hover': {
       textDecoration: 'none',
-      color: theme.palette.text.primary,
-      cursor: 'pointer',
-      '& p': {
-        lineHeight: 'normal',
-      },
-      '&:last-child': {
-        borderBottom: 'none'
-      }
-    }
+    },
+    '&:hover > div,&.active > div': {
+      backgroundColor: 'rgba(30, 111, 214, 0.08)',
+    },
+  },
+  divider: {
+    margin: theme.spacing(0,3)
   }
 }))
 
@@ -46,43 +54,79 @@ const HospitalMap = (props) => {
   const classes = useStyles()
   const [mapStylesLoaded, mapStylesError] = useScript('https://unpkg.com/leaflet@1.6.0/dist/leaflet.css')
   const [mapScriptLoaded, mapScriptError] = useScript('https://unpkg.com/leaflet@1.6.0/dist/leaflet.js')
-  const { t, i18n } = useTranslation(['eligibility','hospitalList'])
+  // const { t, i18n } = useTranslation(['eligibility','hospitalList'])
   // const hospitalData = i18n.getResourceBundle(i18n.languages[0],'hospitalList').hospitals
   const [hospitalData, setHospitalData] = useState([])
   const [map, setMap] = useState()
+  const [markers, setMarkers] = useState([])
+  const [refs, setRefs] = useState([])
 
   useEffect(()=>{
     if(mapScriptLoaded) {
       if(!map){
-        const bssMap = window.L.map('map').setView([38.5561, -90.2496], 5)
-        const OpenStreetMap = window.L.tileLayer('https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=401fa637f2f647f298b4176b24ca7ef5', {
+        const bssMap = window.L.map('map')
+          .locate({setView: true, maxZoom:7}) //local view based on user's geolocation
+          // .setView([38.5561, -90.2496], 5) //wide view from central USA
+          
+        const mapTiles = window.L.tileLayer('https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=401fa637f2f647f298b4176b24ca7ef5', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               });
   
-        OpenStreetMap.addTo(bssMap)
+        mapTiles.addTo(bssMap)
+
+        const updateList = (e) => {
+          const i = e.popup._source.options.i
+          if(e.type === 'popupclose') {
+            refs.map((ref) => {
+              ref.current.classList.remove('active')
+            })
+          }
+          else {
+            refs[i].current.classList.add('active')
+            refs[i].current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'nearest',
+            })
+          }
+        }
+
+        bssMap.on('popupopen popupclose',updateList)
         setMap(bssMap)
       }
     }
   },[mapScriptLoaded])
 
   useEffect(() => {
-    const clickZoom = (e) => {
-      map.setView(e.target.getLatLng(),11);
-    }
-    
+    // const clickZoom = (e) => {
+    //   map.setView(e.target.getLatLng(),11);
+    // }
+
     if(map && hospitalData.length > 0) {
+      let pins = []
       Object.keys(hospitalData).map((item, i) => {
         const hospital = hospitalData[item]
         const gpsMarker = hospital.gps_coordinates.split(",")
-        window.L.marker(gpsMarker,{
-          title: hospital.title
-        }).addTo(map).bindPopup(hospital.title)
+        const thisMarker = window.L.marker(gpsMarker,{
+          title: hospital.title,
+          i
+        })
+        pins.push(thisMarker)
+        thisMarker.addTo(map).bindPopup(hospital.title)
+        setMarkers(prev => [...prev,thisMarker])
         // .on('click',clickZoom); 
       })
-    }
 
-    
+      // const bounds = window.L.latLngBounds(pins)
+      // const group = new window.L.featureGroup(pins);
+      // map.fitBounds(group.getBounds());
+
+      // setRefs(hospitalData.reduce((acc, value, i) => {
+      //     acc[i] = React.createRef();
+      //     return acc;
+      //   }, {})
+      // )
+    }
   }, [map, hospitalData])
 
   useEffect(() => {
@@ -91,19 +135,33 @@ const HospitalMap = (props) => {
         if(resp instanceof Error) {
           throw resp
         }
+        Object.keys(resp).map((item, i) => {
+          setRefs(prev => [...prev,React.createRef()])
+        })
         setHospitalData(resp)
       })
     })
     .catch(error => {
       console.error(error)
     })
-        
   }, [])
 
   const updateMap = (e) => {
     e.preventDefault()
     const cords = e.currentTarget.dataset.location.split(",")
+    const i = e.currentTarget.dataset.index
+    refs.map((ref) => {
+      ref.current.classList.remove('active')
+    })
+    refs[i].current.classList.add("active")
+
+    refs[i].current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+    })
+
     map.setView(cords,11)
+    markers[i].openPopup()
   }
 
   return (
@@ -118,17 +176,24 @@ const HospitalMap = (props) => {
               {
                 hospitalData && Object.keys(hospitalData).map((item, i) => {
                   const hospital = hospitalData[item]
+                  // console.log(refs[i])
                   return (
-                    <Link key={i} data-location={hospital.gps_coordinates} onClick={updateMap}>
-                      <Typography className={classes.title}>{hospital.title}</Typography>
-                      <Typography>
-                        {hospital.address_1}<br />
-                        {hospital.address_2 && <>{hospital.address_2} <br /></>}
-                        {hospital.city}, {hospital.state} {hospital.zip}<br />
-                        {hospital.poc && <>{hospital.poc} <br /></>}
-                        {formatPhoneNumber(hospital.telephone)}
-                      </Typography>
-                    </Link>
+                    <React.Fragment key={i}>
+                      <Link className={classes.location} ref={refs[i]} data-location={hospital.gps_coordinates} onClick={updateMap} data-index={i}>
+                        <Box>
+                          <Typography className={classes.title}>{hospital.title}</Typography>
+                          <Typography>
+                            {hospital.address_1}<br />
+                            {hospital.address_2 && <>{hospital.address_2} <br /></>}
+                            {hospital.city}, {hospital.state} {hospital.zip}<br />
+                            {hospital.poc && <>{hospital.poc} <br /></>}
+                            {hospital.poc_email && <>{hospital.poc_email} <br /></>}
+                            {formatPhoneNumber(hospital.telephone)} {hospital.extension && <>x{hospital.extension}</>}
+                          </Typography>
+                        </Box>
+                      </Link>
+                      {i < hospitalData.length-1 && <Divider className={classes.divider} />}
+                    </React.Fragment>
                   )
                 })
               }
