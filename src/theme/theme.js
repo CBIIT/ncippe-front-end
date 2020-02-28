9f944129cddf86fd5d3556911b1b4ef36f325c1d
alifr <frank.ali@nih.gov>
2020-02-27 20:29:30 -0500

    fix Safari gradients for transparent

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -59,9 +59,9 @@ export const theme = createMuiTheme({
   },
   gradients: {
     primary: 'linear-gradient(#d2e2f7, #facfdc)',
-    blue: 'linear-gradient(#d2e2f7, transparent)',
-    lightBlue: 'linear-gradient(#eff4fb, transparent 40%)',
-    blueReverse: 'linear-gradient(transparent, #f0f5fb)',
+    blue: 'linear-gradient(#d2e2f7, rgba(255,255,255,0))',
+    lightBlue: 'linear-gradient(#eff4fb, rgba(255,255,255,0) 40%)',
+    blueReverse: 'linear-gradient(rgba(255,255,255,0), #f0f5fb)',
     primaryDiagonal: 'linear-gradient(315deg, #FFEBF1 0%, #F3EEF6 51%, #E8F0FA 100%)'
   },
   text: {
