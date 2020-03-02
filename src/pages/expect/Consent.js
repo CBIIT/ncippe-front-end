faecac4249c64fd3dc9a95f40de764f471a04997
Debapriya Sarkar <deba.sarkar@gmail.com>
2020-03-02 17:56:09 -0500

    NCIPPE-185 NCIPPE| What to expect| 'Review a consent form' link should redirect to sample on Give your consent tab econsent form Fixed the sample forms

--- src/pages/expect/Consent.js
+++ src/pages/expect/Consent.js
@@ -102,13 +102,13 @@ const Consent = () => {
           <Grid item xs={12} md={6} lg={8} className={classes.textColumn}>
             <Box mb={5} className={classes.linkList}>
               <Typography variant="h3" component="h3">{t('sample_title')}</Typography>
-              <Button href="https://www.youtube.com/watch?v=iSKqg50b5oc" color="primary" rel="noopener noreferrer" target="_blank">
+              <Button  href={`/${process.env.PUBLIC_URL}assets/documents/Sample-Adult-Consent-Form.pdf`} color="primary" rel="noopener noreferrer" target="_blank">
                 <RenderContent source={t('form_link_adult')} />
               </Button>
-              <Button href="https://www.youtube.com/watch?v=iSKqg50b5oc" color="primary" rel="noopener noreferrer" target="_blank">
+              <Button  href={`/${process.env.PUBLIC_URL}assets/documents/Sample-Parental-Permission-Form.pdf`} color="primary" rel="noopener noreferrer" target="_blank">
                 <RenderContent source={t('form_link_parental')} />
               </Button>
-              <Button href="https://www.youtube.com/watch?v=iSKqg50b5oc" color="primary" rel="noopener noreferrer" target="_blank">
+              <Button  href={`/${process.env.PUBLIC_URL}assets/documents/Sample-Assent-Form-For-Minors.pdf`} color="primary" rel="noopener noreferrer" target="_blank">
                 <RenderContent source={t('form_link_minors')} />
               </Button>
             </Box>
