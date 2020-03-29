57b56246a3945e28595f8783049869278e2c8882
alifr <frank.ali@nih.gov>
2020-03-29 14:42:02 -0400

    add responsive styles for stepper

--- src/components/Mocha/UploadStepper.js
+++ src/components/Mocha/UploadStepper.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Stepper, Typography, Step, StepLabel } from '@material-ui/core';
+import { Stepper, Typography, Step, StepLabel, StepContent, useMediaQuery } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
 import { useTranslation } from 'react-i18next'
+import { theme } from '../../theme/theme';
 
 const useStyles = makeStyles( theme => ({
   stepper: {
     marginTop: theme.spacing(2),
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
+    [`@media (max-width: 799px)`]: {
+      marginTop: 0,
+      padding: theme.spacing(2,0)
+    }
+  },
+  label: {
+    margin: "0 !important",
   }
 }))
 
@@ -16,6 +24,8 @@ const UploadStepper = (props) => {
   const {activeStep = 0} = props
   const classes = useStyles()
   const { t } = useTranslation('a_landingMocha')
+  const orientation = useMediaQuery('@media (max-width: 799px)') ? "vertical" : "horizontal"
+  const labelOrientation = useMediaQuery('@media (max-width: 799px)') ? false : true
 
   const stepText = [
     t('stepper.0.label'),
@@ -26,10 +36,11 @@ const UploadStepper = (props) => {
   return (
     <div className={classes.root}>
       <Typography variant="h3" component="h3">{t('stepper_title')}</Typography>
-      <Stepper className={classes.stepper} activeStep={activeStep} alternativeLabel>
+      <Stepper className={classes.stepper} activeStep={activeStep} alternativeLabel={labelOrientation} orientation={orientation}>
         {stepText.map((text, i) => (
           <Step key={i}>
-            <StepLabel><Typography >{text}</Typography></StepLabel>
+            <StepLabel><Typography className={classes.label}>{text}</Typography></StepLabel>
+            <StepContent /> {/* StepContent item is needed for vertical spacing} */}
           </Step>
         ))}
       </Stepper>
