cfaf1bfaf59c6335727377eae292a742b8cf32af
alifr <frank.ali@nih.gov>
2020-04-15 12:58:07 -0400

    fix attribute typo

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default (props) => (
   <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
-    <img srcset={`/assets/images/spinner-dna.svg`} src={`/assets/images/spinner-dna.gif`} alt="loading indicator" width="250px" style={{width:250,height:250,margin:'auto 0'}} />
+    <img srcSet={`/assets/images/spinner-dna.svg`} src={`/assets/images/spinner-dna.gif`} alt="loading indicator" width="250px" style={{width:250,height:250,margin:'auto 0'}} />
     { props.message && <span>{props.message}</span>}
   </div>
 )
