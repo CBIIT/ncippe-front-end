2122e0f77bd559c674df8022f6b4f0bd72a5c91d
alifr <frank.ali@nih.gov>
2020-05-19 19:33:23 -0400

    Move Research Page. Add research article Page Update routes to new research landing page. Add dynamic route for articles Add utility to match path to translated resource name

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -180,3 +180,46 @@ export const check_webp_feature = (feature, callback) => {
   };
   img.src = "data:image/webp;base64," + kTestImages[feature];
 }
+
+/**
+ * Convert camel case strings to hyphenated lowercase strings
+ * 
+ * @param {string} string - camel cased string to convert
+ * @return {string}
+ */
+export const convertToKebab = (string) => string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+/**
+ * Convert hyphenated strings to camel case strings
+ * 
+ * @param {string} string - hyphenated string to convert
+ * @param {boolean} pascal - capitalize the first letter in the string
+ * @return {string}
+ */
+export const convertToCamel = (string, pascal = false) => {
+	const converter = (matches) => matches[1].toUpperCase();
+	let result = string.replace(/(\-\w)/g, converter);
+
+	if ( pascal === true ) {
+		result = result.charAt(0).toUpperCase() + result.slice(1);
+	}
+
+	return result;
+}
+
+/**
+ * Autodetect string conversion from camel case to hyphenated and vice versa
+ * 
+ * @param {string} string - string to convert
+ * @param {boolean} pascal - capitalize the first letter in the string
+ * @return {string}
+ */
+export const caseConverter = (string, pascal = false) => {
+	if (string.match(/(\-\w)/)) {
+    return convertToCamel(string, pascal)
+  } else if (string.match(/([a-z])([A-Z])/g)) {
+    return convertToKebab(string)
+  } else {
+    return string;
+  }
+} 
