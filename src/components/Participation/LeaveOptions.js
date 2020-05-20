5bad951ff61229eb908da83d8ea2398f587cc439
alifr <frank.ali@nih.gov>
2020-05-20 12:24:58 -0400

    update responsive styles for change participation

--- src/components/Participation/LeaveOptions.js
+++ src/components/Participation/LeaveOptions.js
@@ -15,12 +15,13 @@ const useStyles = makeStyles( theme => ({
   formButtons: {
     marginBottom: theme.spacing(2)
   },
-  btnCancel: {
-    marginLeft: theme.spacing(1)
+  btnSubmit: {
+    marginRight: theme.spacing(1)
   }
 }))
 
 const LeaveOptions = (props) => {
+  const {isMobile} = props
   const classes = useStyles()
   const { t } = useTranslation(['a_changeParticipation','a_common'])
   const { trackEvent } = useTracking()
@@ -36,12 +37,12 @@ const LeaveOptions = (props) => {
 
   return (
     <Box>
-      <Typography className={classes.header} variant="h1" component="h1">{t('leave.0.pageTitle')}</Typography>
-      <Typography variant="h2" className={classes.gutterBottom_2}>{t('leave.0.subtitle')}</Typography>
+      <Typography className={classes.header} variant={isMobile ? "h2" : "h1"} component="h1">{t('leave.0.pageTitle')}</Typography>
+      <Typography variant={isMobile ? "h3" : "h2"} className={classes.gutterBottom_2}>{t('leave.0.subtitle')}</Typography>
       <Typography component="div"><RenderContent source={t('leave.0.body')} /></Typography>
       <div className={classes.formButtons}>
-        <Button variant="contained" color="primary" component={RouterLink} to='../leaveQuestions' onClick={trackClick}>{t('leave.0.submit')}</Button>
-        <Button className={classes.btnCancel} variant="text" onClick={props.cancel}><ClearIcon />{t('a_common:buttons.cancel')}</Button>
+        <Button className={classes.btnSubmit} variant="contained" color="primary" component={RouterLink} to='../leaveQuestions' onClick={trackClick}>{t('leave.0.submit')}</Button>
+        <Button variant="text" onClick={props.cancel}><ClearIcon />{t('a_common:buttons.cancel')}</Button>
       </div>
     </Box>
   )
