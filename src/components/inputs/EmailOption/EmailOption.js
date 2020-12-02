cecfc53c41458616bd03fb2c6bd5faae605deb36
alifr <frank.ali@nih.gov>
2020-12-02 12:06:16 -0500

    fix import

--- src/components/inputs/EmailOption/EmailOption.js
+++ src/components/inputs/EmailOption/EmailOption.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import Checkbox from '@material-ui/core/Checkbox'
 import PropTypes from 'prop-types'
-import { FormControl, FormLabel, FormControlLabel } from '@material-ui/core'
+import { Checkbox, FormControl, FormLabel, FormControlLabel } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { useTranslation } from 'react-i18next'
 
