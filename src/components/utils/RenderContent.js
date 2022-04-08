60bf1116c9ae4da70184730c9d5f8ab692c59339
alifr <frank.ali@publicissapient.com>
2022-04-08 13:56:08 -0400

    fix to work with upgraded react-markdown module

--- src/components/utils/RenderContent.js
+++ src/components/utils/RenderContent.js
@@ -1,22 +1,7 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 
-const root = props => {
-  console.log(props)
-  if(props?.children[0] && props.children[0].props?.children) {
-    return props.children[0].props?.children
-  }
-  if(props?.children[0] && props.children[0].props?.element) {
-    return props.children[0].props?.element
-  }
-  if(props.children) {
-    return props.children
-  }
-}
-
-const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} components={{
-  p: ({node, ...props}) => props.children
-}} />
+const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} />
 
 export default RenderContent
