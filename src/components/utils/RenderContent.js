c29483e86ad95df6670fdc86276ce1bbf9bbdd9e
alifr <frank.ali@publicissapient.com>
2022-07-28 13:28:03 -0400

    fix code issues with package versions

--- src/components/utils/RenderContent.js
+++ src/components/utils/RenderContent.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 
-const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} />
+const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} components={{
+  p: ({node, ...props}) => props.children
+}} />
 
 export default RenderContent
