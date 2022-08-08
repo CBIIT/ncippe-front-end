8030a7818abfa1301037e5923114a423034ef45a
alifr <frank.ali@publicissapient.com>
2022-08-08 17:52:33 -0400

    finally a fix for removing root paragraphs from markdown

--- src/components/utils/RenderContent.js
+++ src/components/utils/RenderContent.js
@@ -2,8 +2,32 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 
-const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} components={{
-  p: ({node, ...props}) => props.children
-}} />
+// strip all <p> tags
+// const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} components={{
+//   p: ({node, ...props}) => props.children
+// }} />
+
+// const RenderContent =  (props) => <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} components={{
+//   p: React.Fragment,
+// }} />
+
+const RenderContent =  (props) => {
+
+  // remove wrapping paragraphs made by markdown, but keep paragraphs that we have in our authored content
+  const removeRootParagraph = ({node,...props}) => {
+    const {children} = props
+
+    if(node.position.start.offset === node.children[0].position.start.offset) {
+      return children
+    }
+    // if start.offsets do not match, then this node was processed by rehypeRaw and the HTML tags at the start of the text node account for different start positions
+    return <p>{children}</p>
+  }
+
+  return <ReactMarkdown {...props} rehypePlugins={[rehypeRaw]} components={{
+      p: removeRootParagraph,
+  }} />
+}
+
 
 export default RenderContent
