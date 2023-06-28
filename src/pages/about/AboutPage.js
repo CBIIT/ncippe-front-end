9209466e910562bc4e845f38fbf5ee1bde99ed5b
Your Name <you@example.com>
2023-06-28 11:52:49 -0400

    chart api testing

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -66,8 +66,6 @@ const AboutPage = () => {
         getAPI.then(api => {
             console.log('MHL 01b handleClick');
             api.getChartData().then(resp => {
-                console.log('MHL 01d chartData typeof: ', typeof resp);
-                console.log('MHL 01e chartData filter: ', resp.filter);
                 chartData = resp;
                 console.log('MHL 01c chartData: ', chartData);
                 if (resp instanceof Error) {
