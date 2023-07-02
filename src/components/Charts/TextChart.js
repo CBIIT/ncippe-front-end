e876c6569d7d267d6ed3261cb0cf744293e8eb2f
Your Name <you@example.com>
2023-07-02 11:50:01 -0400

    chart api testing

--- src/components/Charts/TextChart.js
+++ src/components/Charts/TextChart.js
@@ -3,6 +3,7 @@ import {useTranslation} from "react-i18next";
 import '../../index.css'
 import {Typography} from "@material-ui/core";
 import RenderContent from "../utils/RenderContent";
+import getAPI from "../../data";
 
 const TextChart = (props) => {
     console.log('MHL TextChart props: ', props );
@@ -20,12 +21,48 @@ const TextChart = (props) => {
 
     const node = 'textChart-container' + svgId.toString();
     // @TODO Move this to Chart.js
+/*
     let data = [
         {label: t('charts.chart_data.ProjectSummary.ParticipantsEnrolled') , value: 26},
         {label: t('charts.chart_data.ProjectSummary.SitesThatHaveEnrolledParticipants'), value: 21},
         {label: t('charts.chart_data.ProjectSummary.CancerTypes'), value: 18},
         {label: t('charts.chart_data.ProjectSummary.BiomarkerTestReturned'), value: 16}
     ];
+*/
+
+    let data = '{}';
+    getAPI.then(api => {
+
+        console.log('MHL 614a drawBarChart00');
+        api.getChartData3().then(resp => {
+            console.log('MHL 614b handleClick3 resp: ', resp);
+            if (resp instanceof Error) {
+                console.error('MHL 615c getChartData3 error: ', resp);
+            }
+            if (svgId === 1) {
+                data = resp['projectSummary'];
+                console.log('MHL 615d getChartData1 svgId: ', svgId);
+                console.log('MHL 615e getChartData3 data: ', data);
+
+            }
+
+            translateLabels(data);
+
+            console.log('MHL 1 BarChart data: ', data);
+        })});
+    // @FIXME dup code
+    function translateLabels(obj) {
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === 'object' && obj[key] !== null) {
+                    translateLabels(obj[key]); // recursive
+                } else {
+                    obj[key] = t(obj[key]);
+                }
+            }
+        }
+    }
+
 
     return (
         <div className={'div-chart'}>
