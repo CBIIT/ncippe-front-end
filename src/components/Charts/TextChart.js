7315ffcc1a8a12698a16352df6d2e9234d698980
Your Name <you@example.com>
2023-07-02 12:08:36 -0400

    chart api testing

--- src/components/Charts/TextChart.js
+++ src/components/Charts/TextChart.js
@@ -39,9 +39,9 @@ const TextChart = (props) => {
             if (resp instanceof Error) {
                 console.error('MHL 615c getChartData3 error: ', resp);
             }
+            console.log('MHL 615d getChartData1 svgId: ', svgId);
             if (svgId === 0) {
                 data = resp['projectSummary'];
-                console.log('MHL 615d getChartData1 svgId: ', svgId);
                 console.log('MHL 615e getChartData3 data: ', data);
             }
 
