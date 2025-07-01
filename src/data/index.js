81874c2ad4593cad9cd4e991603282446412ab07
Chao Zhang <chao.zhang3@nih.gov>
2025-07-01 08:10:10 -0400

    lib updates for node v22

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,14 @@
 export default import(/* webpackMode: "eager" */ `../data/${process.env.REACT_APP_API_PATH}/api`).then(module => module.api)
 
 export async function getAPI(){
-  const module = await import(/* webpackMode: "eager" */ `../data/${process.env.REACT_APP_API_PATH}/api`)
-  return module.api
+  const path = process.env.REACT_APP_API_PATH;
+  if(!path) {
+    console.log('API path is not defined');
+    const module = await import(/* webpackMode: "eager" */ `../data/prod/api`)
+    return module.api
+  }else {
+    const module = await import(/* webpackMode: "eager" */ `../data/${path}/api`)
+    return module.api
+  }
+  
 }
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,6 +21,8 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+   console.table(Object.entries(process.env).filter(([key]) => key.startsWith('REACT_APP_')));
+
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
