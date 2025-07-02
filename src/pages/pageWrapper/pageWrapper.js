d9949f546f91c9bb668e38fcbf67c585b82a9712
Chao Zhang <chao.zhang3@nih.gov>
2025-07-01 23:29:59 -0400

    lib updates for node v22

--- src/pages/pageWrapper/pageWrapper.js
+++ src/pages/pageWrapper/pageWrapper.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Box } from '@mui/material'
 import Header from '../../components/region/Header/Header'
 import Footer from '../../components/region/Footer/Footer'
 
 const pageWrapper = (Component) => ({children, ...props}) => {
   return (
-    <div className="siteWrapper">
+    <Box className="siteWrapper" sx={{ display: "flex",
+     flexDirection:"column", minHeight:"100vh" }}>
       <Header />
-      <div id="main" sx={{ flex: '1 0 auto',
+      <Box id="main" component="main"  sx={{ flex: "1 0 auto",
          margin: '0', }} role="main">
         <Component {...props} />
-      </div>
+      </Box>
       <Footer />
-    </div>
+    </Box>
   )
 }
 
